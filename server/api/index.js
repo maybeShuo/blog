65d7cdfe0bee05e2da2484ef9278e3c1846bb046
maybeShuo <546537825@qq.com>
2017-11-24 23:35:30 +0800

    Add mongodb config.

--- server/api/index.js
+++ server/api/index.js
@@ -1,2 +1,34 @@
-require('babel-register')
-require('./apiServer')
+/**
+  * api请求server
+  */
+import Express from 'express'
+import mongoose from 'mongoose'
+import config from '../../config/config'
+import bodyParser from 'body-parser'
+
+const port = config.apiPort
+
+const app = new Express()
+
+app.use('/', require('./main'))
+app.use('/admin', require('./admin'))
+
+
+mongoose.Promise = require('bluebird')
+mongoose.connect(`mongodb://${config.dbHost}:${config.dbPort}/blog`, err => {
+    if(err) {
+        console.log(err, '数据库连接失败')
+        return
+    }
+
+    console.log('数据库连接成功')
+        
+    app.listen(port, err => {
+        if(err) {
+            console.error('err:', err)
+        } else {
+            console.info(`===> api server is running at ${config.apiHost}:${config.apiPort}`)
+        }
+    })
+    
+})
